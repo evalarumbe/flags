e225d231457b29fc4092e56b6094d1130445dd1f
Eva Larumbe <evalarumbe@gmail.com>
2019-12-21 09:39:41 +0100

    Encapsulate flags

--- script.js
+++ script.js
@@ -5,69 +5,69 @@ const c = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-/* sizing */
-const size = 100;
-const gap = size / 20;
+const drawFlags = (size, gap) => {
+    /* colors */
 
-/* colors */
+    const drawE = (xOffset = 0) => {
+        // top part, blue
+        c.fillStyle = `rgb(0, 0, 255)`;
+        c.fillRect(xOffset, 0, size, size / 2);
+        // bottom part, red
+        c.fillStyle = `rgb(255, 0, 0)`;
+        c.fillRect(xOffset, (size / 2), size, size / 2);
+    };
 
-const drawE = (xOffset = 0) => {
-    // top part, blue
-    c.fillStyle = `rgb(0, 0, 255)`;
-    c.fillRect(xOffset, 0, size, size / 2);
-    // bottom part, red
-    c.fillStyle = `rgb(255, 0, 0)`;
-    c.fillRect(xOffset, (size / 2), size, size / 2);
-};
+    const drawV = (xOffset = 0) => {
+        // background, white
+        c.fillStyle = `rgb(255, 255, 255)`;
+        c.fillRect(xOffset, 0, size, size);
+        // X, red
+        c.fillStyle = `rgb(255, 0, 0)`;
+        c.beginPath();
+        // top
+        c.moveTo(xOffset, 0);
+        c.lineTo(xOffset + size / 6, 0);
+        c.lineTo(xOffset + size / 2, size / 3);
+        c.lineTo(xOffset + size * 5 / 6, 0);
+        c.lineTo(xOffset + size, 0);
+        // right
+        c.lineTo(xOffset + size, size / 6);
+        c.lineTo(xOffset + size * 2 / 3, size / 2);
+        c.lineTo(xOffset + size, size * 5 / 6);
+        c.lineTo(xOffset + size, size);
+        // bottom
+        c.lineTo(xOffset + size * 5 / 6, size);
+        c.lineTo(xOffset + size / 2, size * 2 / 3);
+        c.lineTo(xOffset + size / 6, size);
+        c.lineTo(xOffset, size);
+        // // left
+        c.lineTo(xOffset, size * 5 / 6);
+        c.lineTo(xOffset + size / 3, size / 2);
+        c.lineTo(xOffset, size / 6);
+        c.lineTo(xOffset, 0);
+        c.fill();
+    };
 
-const drawV = (xOffset = 0) => {
-    // background, white
-    c.fillStyle = `rgb(255, 255, 255)`;
-    c.fillRect(xOffset, 0, size, size);
-    // X, red
-    c.fillStyle = `rgb(255, 0, 0)`;
-    c.beginPath();
-    // top
-    c.moveTo(xOffset, 0);
-    c.lineTo(xOffset + size / 6, 0);
-    c.lineTo(xOffset + size / 2, size / 3);
-    c.lineTo(xOffset + size * 5 / 6, 0);
-    c.lineTo(xOffset + size, 0);
-    // right
-    c.lineTo(xOffset + size, size / 6);
-    c.lineTo(xOffset + size * 2 / 3, size / 2);
-    c.lineTo(xOffset + size, size * 5 / 6);
-    c.lineTo(xOffset + size, size);
-    // bottom
-    c.lineTo(xOffset + size * 5 / 6, size);
-    c.lineTo(xOffset + size / 2, size * 2 / 3);
-    c.lineTo(xOffset + size / 6, size);
-    c.lineTo(xOffset, size);
-    // // left
-    c.lineTo(xOffset, size * 5 / 6);
-    c.lineTo(xOffset + size / 3, size / 2);
-    c.lineTo(xOffset, size / 6);
-    c.lineTo(xOffset, 0);
-    c.fill();
-};
+    const drawA = (xOffset = 0) => {
+        // left part, white
+        c.fillStyle = `rgb(255, 255, 255)`;
+        c.fillRect(xOffset, 0, size / 2, size);
 
-const drawA = (xOffset = 0) => {
-    // left part, white
-    c.fillStyle = `rgb(255, 255, 255)`;
-    c.fillRect(xOffset, 0, size / 2, size);
+        // right part, blue
+        c.fillStyle = `rgb(0, 0, 255)`;
+        c.beginPath();
+        c.moveTo(xOffset + size * 0.5, 0);
+        c.lineTo(xOffset + size, 0);
+        c.lineTo(xOffset + size * 0.75, size / 2);
+        c.lineTo(xOffset + size, size);
+        c.lineTo(xOffset + size * 0.5, size);
+        c.lineTo(xOffset + size * 0.5, 0);
+        c.fill();
+    };
 
-    // right part, blue
-    c.fillStyle = `rgb(0, 0, 255)`;
-    c.beginPath();
-    c.moveTo(xOffset + size * 0.5, 0);
-    c.lineTo(xOffset + size, 0);
-    c.lineTo(xOffset + size * 0.75, size / 2);
-    c.lineTo(xOffset + size, size);
-    c.lineTo(xOffset + size * 0.5, size);
-    c.lineTo(xOffset + size * 0.5, 0);
-    c.fill();
-};
+    drawE(200);
+    drawV(200 + gap + size);
+    drawA(200 + (gap * 2) + (size * 2));
+}
 
-drawE(200);
-drawV(200 + gap + size);
-drawA(200 + (gap * 2) + (size * 2));
+drawFlags(200, 15);
