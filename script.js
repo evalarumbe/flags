6353a5b4b233580203a0870559ca183195a4cfca
Eva Larumbe <evalarumbe@gmail.com>
2019-12-21 20:16:18 +0100

    WIP - tidy and comment

--- script.js
+++ script.js
@@ -4,6 +4,54 @@ const c = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+// wave logic
+const drawWaves = (wavelength) => {
+    const yGap = wavelength * 2.5;  // this proportion just happens to look nice
+    let yOffset = Math.floor(yGap / 2); // start with only a small space up top
+    const maxRows = Math.ceil(canvas.height / (wavelength * 2));
+    let rowIndex = 0;
+
+    const drawRow = (rowIndex) => {
+        // periods = number of wavelengths drawn per wave
+        const periods = [2, 3, 5];
+        const getPeriods = () => periods[Math.floor(Math.random() * 3)];
+        let periodCount = getPeriods();
+        let xOffset = (rowIndex % 2 === 0) ? (- wavelength) : wavelength; // start offscreen for odd rows
+        const xGap = wavelength * 3;
+        let waveWidth = 0;
+
+        const drawWave = (x, y, periods) => {
+            for (let i = 0; i < periods; i++) {
+                c.beginPath();
+                c.arc(x, y, wavelength / 2, 0, Math.PI, false);
+                c.strokeStyle = 'rgba(0, 0, 255)';
+                c.stroke();
+
+                x += wavelength;
+            }
+        };
+
+        for (let widthDrawn = 0; widthDrawn < canvas.width; widthDrawn += waveWidth) {
+            drawWave(xOffset, yOffset, periodCount);
+            // update the waveWidth to stop looping when the screen is full
+            waveWidth = (wavelength * periodCount) + xGap;
+            // update the xOffset so the next wave knows where to start
+            xOffset += (wavelength * periodCount) + xGap;
+            // update the perdioCount so the next wave knows how long to be
+            periodCount = getPeriods();
+        }
+    };
+
+    for (let i = 0; i < maxRows; i++) {
+        drawRow(rowIndex);
+        yOffset += yGap;
+        rowIndex += 1;
+    }
+};
+
+drawWaves(50);
+
+// flag logic
 const flags = [
     {
         letter: 'e',
@@ -78,52 +126,6 @@ const flagsWidth = (flagSize * flags.length) + (flagGap * (flags.length - 1));
 const flagsXOffset = canvas.width - (canvas.width + flagsWidth) / 2;
 
 const flagsYOffset = canvas.height - (canvas.height + flagSize) / 2;
-
-const drawWaves = (wavelength) => {
-    const yGap = wavelength * 2.5;  // this proportion just happens to look nice
-    let yOffset = Math.floor(yGap / 2); // start with only a small space up top
-    const maxRows = Math.ceil(canvas.height / (wavelength * 2));
-    let rowIndex = 0;
-
-    const drawRow = (rowIndex) => {
-        // periods = number of wavelengths drawn per wave
-        const periods = [2, 3, 5];
-        const getPeriods = () => periods[Math.floor(Math.random() * 3)];
-        let periodCount = getPeriods();
-        let xOffset = (rowIndex % 2 === 0) ? (- wavelength) : wavelength; // start offscreen for odd rows
-        const xGap = wavelength * 3;
-        let waveWidth = 0;
-
-        const drawWave = (x, y, periods) => {
-            for (let i = 0; i < periods; i++) {
-                c.beginPath();
-                c.arc(x, y, wavelength / 2, 0, Math.PI, false);
-                c.strokeStyle = 'rgba(0, 0, 255)';
-                c.stroke();
-
-                x += wavelength;
-            }
-        };
-
-        for (let widthDrawn = 0; widthDrawn < canvas.width; widthDrawn += waveWidth) {
-            drawWave(xOffset, yOffset, periodCount);
-            // update the waveWidth to stop looping when the screen is full
-            waveWidth = (wavelength * periodCount) + xGap;
-            // update the xOffset so the next wave knows where to start
-            xOffset += (wavelength * periodCount) + xGap;
-            // update the perdioCount so the next wave knows how long to be
-            periodCount = getPeriods();
-        }
-    };
-
-    for (let i = 0; i < maxRows; i++) {
-        drawRow(rowIndex);
-        yOffset += yGap;
-        rowIndex += 1;
-    }
-};
-
-drawWaves(50);
 
 flags.forEach((flag, i) => {
     // This freaky logic sets the x offset for each flag based on its index.
